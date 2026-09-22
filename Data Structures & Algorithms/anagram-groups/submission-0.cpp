4dbdb7e5679067a667fe4140858d63e015fe3b9e
Dennis Ngo <66426115+AsianProgram@users.noreply.github.com>
2026-09-22 17:53:26 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        //hashmap
+        unordered_map<string, vector<string>> m;
+        
+        //go through each string
+        for (const auto& s : strs) {
+            vector<int> count(26, 0);
+
+            //for each charater in string increase frequency
+            for (char c : s) {
+                count[c - 'a']++;
+            }
+            string key = to_string(count[0]);
+            for (int i = 1; i < 26; ++i) {
+                key += ',' + to_string(count[i]);
+            }
+            m[key].push_back(s);
+        }
+        vector<vector<string>> result;
+        for (const auto& pair : m) {
+            result.push_back(pair.second);
+        }
+        return result;
+    }
+};
